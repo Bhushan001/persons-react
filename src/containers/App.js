f215868a571ff22af624bcebe20d86b3187c69f6
CauseCode <bootstrap@causecode.com>
2018-04-25 16:20:19 +0530

    added adhoc component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "./../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
-
+import WithClass from "./../hoc/WithClass";
 class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -101,7 +101,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showPersons: true });
@@ -115,7 +115,7 @@ class App extends PureComponent {
           toggled={this.togglePersonsHandler}
         />
         <div>{persons}</div>
-      </div>
+      </WithClass>
     );
   }
 }
