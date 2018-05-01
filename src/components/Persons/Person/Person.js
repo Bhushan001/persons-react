7d6af5a540f37dc80d29dc031ba3fed3b5254708
bhushan001 <bhushan.gadekar@causecode.com>
2018-05-01 15:55:21 +0530

    added proptypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
-
+import WithClass from "./../../../hoc/WithClass";
+import PropTypes from "prop-types";
 class Person extends Component {
   constructor(props) {
     super(props);
     console.log(`[Person.js] constructor is called`, props);
   }
 
-  componentWillMount(){
+  componentWillMount() {
     console.log("[Person.js] componentWillMount Calling");
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log("[Person.js] componentDidMount calling");
-
   }
 
   render() {
     console.log("[Person.js] render method calling");
     return (
-      <div className={classes.Person}>
+      <WithClass classes={classes.Person}>
         <p onClick={this.props.click}>
           Hi, I am {this.props.name}, My age is {this.props.age}
         </p>
@@ -27,9 +27,14 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </WithClass>
     );
   }
 }
-
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 export default Person;
